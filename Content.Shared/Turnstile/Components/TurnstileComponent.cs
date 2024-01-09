f9f46c8dd46d6e1298b6b4318928161b105c978a
DrMelon <drmelon.mail@gmail.com>
2024-01-09 15:44:27 +0000

    Physics-based turnstiles... kinda awkward.

--- Content.Shared/Turnstile/Components/TurnstileComponent.cs
+++ Content.Shared/Turnstile/Components/TurnstileComponent.cs
@@ -1,6 +1,8 @@
-﻿using Content.Shared.Turnstile.Systems;
+﻿using System.Numerics;
+using Content.Shared.Turnstile.Systems;
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
+using Robust.Shared.Physics.Dynamics.Joints;
 using Robust.Shared.Serialization;
 
 namespace Content.Shared.Turnstile.Components;
@@ -8,15 +10,28 @@ namespace Content.Shared.Turnstile.Components;
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true), Access(typeof(TurnstileSystem))]
 public sealed partial class TurnstileComponent : Component
 {
+    #region Spinner Physics
+
     /// <summary>
-    /// The current state of the turnstile -- whether it is idle or rotating.
+    /// The physics-driven spinner entity of the turnstile. It is managed by the TurnstileSystem.
     /// </summary>
-    /// <remarks>
-    /// This should never be set directly, use <see cref="TurnstileSystem.SetState(EntityUid, TurnstileState, TurnstileComponent?)"/> instead.
-    /// </remarks>
     [ViewVariables(VVAccess.ReadOnly)]
     [DataField, AutoNetworkedField]
-    public TurnstileState State = TurnstileState.Idle;
+    public EntityUid SpinnerUid;
+
+    /// <summary>
+    /// Turnstile Spinner prototype ID.
+    /// </summary>
+    [DataField]
+    public string SpinnerPrototype = "TurnstileSpinner";
+
+    /// <summary>
+    /// Turnstile Spinner relative anchor point.
+    /// </summary>
+    [DataField]
+    public Vector2 SpinnerAnchorPoint = new Vector2(0.5f, 0.5f);
+
+    #endregion
 
     #region Sounds
     /// <summary>
@@ -33,13 +48,6 @@ public sealed partial class TurnstileComponent : Component
 
     #endregion
 
-}
-
-[Serializable, NetSerializable]
-public enum TurnstileState : byte
-{
-    Idle,
-    Rotating
 }
 
 [Serializable, NetSerializable]
