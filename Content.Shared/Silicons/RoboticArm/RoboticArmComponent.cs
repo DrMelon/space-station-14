cad83d50c10f0945181882213991102309cc112a
drmelon <drmelon.mail@gmail.com>
2026-01-20 17:22:03 +0000

    Working on a robotic arm. Initial pass at simple prototypes, no systems yet for automated arm use.

---
+++ Content.Shared/Silicons/RoboticArm/RoboticArmComponent.cs
@@ -0,0 +1,11 @@
+namespace Content.Shared.Silicons.RoboticArm;
+
+/// <summary>
+/// This component specifies a Robotic Arm.
+/// </summary>
+/// <see cref="SharedRoboticArmSystem"/>
+[RegisterComponent]
+public sealed partial class RoboticArmComponent : Component
+{
+
+}
